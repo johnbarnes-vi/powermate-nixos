83386ba997bb83b7d73b8c15d7a6231d88c5bfb7
johnbarnes-vi <lj502jr@gmail.com>
2025-01-10 10:40:57 -0600

    dir restructure and add module.nix

--- flake.nix
+++ flake.nix
@@ -1,39 +1,27 @@
 {
-  description = "PowerMate control development environment";
+  description = "Griffin PowerMate Volume Controller for NixOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        
-        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-          evdev
-          pygobject3  # For GLib/Gio
-        ]);
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            pythonEnv
-            pamixer
-            gobject-introspection  # For GI typelib files
-            glib
-            
-            # Development tools
-            git
-            nixpkgs-fmt
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      nixosModules.default = import ./module.nix;
+      
+      packages = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.python3.pkgs.buildPythonApplication {
+          pname = "powermate-controller";
+          version = "0.1.0";
+          src = ./src;
+          propagatedBuildInputs = with nixpkgs.legacyPackages.${system}.python3.pkgs; [
+            evdev
           ];
-
-          shellHook = ''
-            echo "PowerMate development environment activated!"
-            echo "Python version: $(python --version)"
-          '';
         };
-      }
-    );
+      });
+    };
 }
---
+++ module.nix
@@ -0,0 +1,45 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.services.powermate-controller;
+in {
+  options.services.powermate-controller = {
+    enable = mkEnableOption "Griffin PowerMate Volume Controller";
+    
+    package = mkOption {
+      type = types.package;
+      description = "The PowerMate controller package";
+      default = pkgs.python3.pkgs.buildPythonApplication {
+        pname = "powermate-controller";
+        version = "0.1.0";
+        src = ./src;
+        propagatedBuildInputs = with pkgs.python3.pkgs; [
+          evdev
+        ];
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.user.services.powermate-controller = {
+      description = "Griffin PowerMate Volume Controller";
+      wantedBy = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/powermate_controller";
+        Restart = "always";
+        RestartSec = 3;
+      };
+    };
+
+    # Your existing udev rules
+    services.udev.extraRules = ''
+      SUBSYSTEM=="input", ATTRS{idVendor}=="077d", ATTRS{idProduct}=="0410", MODE="0666", GROUP="input"
+    '';
+
+    users.groups.input = {};
+  };
+}
